e4d3ac74e6d5fba77687bf3acfb268e44dfb2697
sreynolds09 <sara.reynolds.clan@gmail.com>
2025-05-28 20:37:41 -0400

    initial commit

---
+++ Module1Review/Module1Review/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Module1Review
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            static double CircleArea(double radius)
+            {
+                return Math.PI * radius * radius;
+            }
+
+            double radius = 0;
+            Console.WriteLine($"What is the radius of the circle?");
+            
+            int userInput = int.Parse(Console.ReadLine());
+            userInput = (int)(radius + userInput);
+            double area = CircleArea(userInput); 
+            Console.WriteLine($"The area of the circle with radius {userInput} is: {area}"); 
+        }
+        
+       
+    }
+}
